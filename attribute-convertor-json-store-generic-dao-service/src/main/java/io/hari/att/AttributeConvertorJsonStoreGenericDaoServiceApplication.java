0aefe0f3ad6e9b0e504352a2f7a78429785a930c
Hariom Yadav <hoc.yadav@gmail.com>
2021-02-23 17:04:28 +0530

    updated main class

--- attribute-convertor-json-store-generic-dao-service/src/main/java/io/hari/att/AttributeConvertorJsonStoreGenericDaoServiceApplication.java
+++ attribute-convertor-json-store-generic-dao-service/src/main/java/io/hari/att/AttributeConvertorJsonStoreGenericDaoServiceApplication.java
@@ -1,7 +1,21 @@
 package io.hari.att;
 
+import io.hari.att.config.MyConfig;
+import io.hari.att.dao.PersonDao;
+import io.hari.att.entity.Address;
+import io.hari.att.entity.AgeType;
+import io.hari.att.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @SpringBootApplication
 public class AttributeConvertorJsonStoreGenericDaoServiceApplication {
@@ -10,4 +24,54 @@ public class AttributeConvertorJsonStoreGenericDaoServiceApplication {
         SpringApplication.run(AttributeConvertorJsonStoreGenericDaoServiceApplication.class, args);
     }
 
+    @Autowired
+    PersonDao personDao;
+
+    @Autowired
+    MyConfig myConfig;
+
+    @PostConstruct
+    public void foo() {
+        final String fieldNameSimple = myConfig.getFieldNameSimple();
+        System.out.println("fieldNameSimple = " + fieldNameSimple);
+
+        final Map<String, String> fieldName = myConfig.getFieldName();
+        System.out.println("fieldName = " + fieldName);
+
+        final Map<String, MyConfig.ClassA> fieldNameObject = myConfig.getFieldNameObject();
+        System.out.println("fieldNameObject = " + fieldNameObject);
+        System.out.println("fieldNameObject keyAd = " + fieldNameObject.get("keyAd"));
+
+        final Map<String, MyConfig.ClassB> fieldNameAdObject = myConfig.getFieldNameAdObject();
+        System.out.println("fieldNameAdObject = " + fieldNameAdObject);
+        final MyConfig.ClassB keyA = fieldNameAdObject.get("keyA");
+        final String name = keyA.getName();
+        System.out.println("name = " + name);
+        final Set<String> places = keyA.getPlaces();
+        System.out.println("places = " + places);
+        final Map<String, String> keyValues = keyA.getKeyValues();
+        System.out.println("keyValues = " + keyValues);
+
+
+
+        System.err.println("AttributeConvertorJsonStoreGenericDaoServiceApplication.foo");
+
+        personDao.save(Person.builder().name("hari").ageType(AgeType.GENERATION2)
+                .dob(LocalDate.of(1989, Month.JUNE, 26))
+                .address(Address.builder().pincode("495452").build()).build());
+
+        final List<Person> people = personDao.findAll();
+        people.forEach(i -> {
+            System.out.println("i = " + i);
+        });
+
+        final Person person = people.get(0);
+        System.out.println("person = " + person);
+        final Address address = person.getAddress();
+        System.out.println("address = " + address);
+
+        final List<Person> allByAgeType = personDao.findAllByAgeType(AgeType.GENERATION1);
+        System.out.println("allByAgeType = " + allByAgeType);
+
+    }
 }
