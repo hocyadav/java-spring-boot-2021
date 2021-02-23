9cf2d31eec6bf80b0ebf2326a4a463c52154a884
Hariom Yadav <hoc.yadav@gmail.com>
2021-02-23 15:58:24 +0530

    created new spring boot folder

---
+++ attribute-convertor-json-store-generic-dao-service/src/main/java/io/hari/att/AttributeConvertorJsonStoreGenericDaoServiceApplication.java
@@ -0,0 +1,13 @@
+package io.hari.att;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttributeConvertorJsonStoreGenericDaoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttributeConvertorJsonStoreGenericDaoServiceApplication.class, args);
+    }
+
+}
---
+++ attribute-convertor-json-store-generic-dao-service/src/test/java/io/hari/att/AttributeConvertorJsonStoreGenericDaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hari.att;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttributeConvertorJsonStoreGenericDaoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
