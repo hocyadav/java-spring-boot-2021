398a5aa87e8f27819a20926e3850a2ae50843a88
Hariom Yadav <hoc.yadav@gmail.com>
2021-02-24 16:28:07 +0530

    created new project for testing

---
+++ spring-boot-testing/src/main/java/io/hari/testing/SpringBootTestingApplication.java
@@ -0,0 +1,13 @@
+package io.hari.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestingApplication.class, args);
+    }
+
+}
