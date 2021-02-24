0590aaaaf7c611e9fc81c9402a633ecc2b05cc60
Hariom Yadav <hoc.yadav@gmail.com>
2021-02-24 17:09:43 +0530

    advance spring boot test impl

---
+++ spring-boot-testing/src/test/java/io/hari/testing/MyService2Test_adv.java
@@ -0,0 +1,42 @@
+package io.hari.testing;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest // all beans loaded -> then below autowire takes place
+class MyService2Test_adv {
+
+    @Autowired
+    MyService2 myService2;
+
+    @Autowired
+    TaxBracketService taxBracketService;
+
+    @BeforeEach
+    void setUp() {
+        //not required constructor call
+    }
+
+    @Test
+    void calculateTax() {
+        final String tax = myService2.calculateTax(499);
+        assertThat(tax).isEqualTo("LOW");
+    }
+
+    @Test
+    void allTaxBracket() {
+        final List<String> allTaxBracket = myService2.allTaxBracket();
+        assertThat(allTaxBracket)
+                .contains("LOW", "MEDIAM", "HIGH")
+                .doesNotContain("XYZ");
+    }
+}
