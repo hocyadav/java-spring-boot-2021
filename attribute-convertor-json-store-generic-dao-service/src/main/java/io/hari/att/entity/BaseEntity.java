388677f997040b1e7ca771668ee34084088aa204
Hariom Yadav <hoc.yadav@gmail.com>
2021-02-24 18:52:12 +0530

    updated

--- attribute-convertor-json-store-generic-dao-service/src/main/java/io/hari/att/entity/BaseEntity.java
+++ attribute-convertor-json-store-generic-dao-service/src/main/java/io/hari/att/entity/BaseEntity.java
@@ -3,6 +3,7 @@ package io.hari.att.entity;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
@@ -13,4 +14,13 @@ public class BaseEntity {
 
     @Version
     Long version;
+
+    LocalDateTime createAt;
+
+    @PrePersist
+    public void foo() {
+          if(createAt == null) {
+              createAt = LocalDateTime.now();
+          }
+    }
 }
