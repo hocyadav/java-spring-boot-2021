48ea137c7ee2355c3516a67618efcc53a74438fa
Hariom Yadav <hoc.yadav@gmail.com>
2021-02-24 17:58:54 +0530

    updated

---
+++ spring-boot-testing/src/test/java/io/hari/testing/MyService2Test_adv_2.java
@@ -0,0 +1,38 @@
+package io.hari.testing;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@ContextConfiguration(classes = {MyService2.class, TaxBracketService.class})// only loading 2 bean
+class MyService2Test_adv_2 {
+
+    @Autowired
+    MyService2 myService2;
+
+    @Autowired
+    TaxBracketService taxBracketService;
+
+    @Test
+    void calculateTax() {
+        final String tax = myService2.calculateTax(499);
+        assertThat(tax).isEqualTo("LOW");
+    }
+
+    @Test
+    void allTaxBracket() {
+        final List<String> allTaxBracket = myService2.allTaxBracket();
+        assertThat(allTaxBracket)
+                .contains("LOW", "MEDIAM", "HIGH")
+                .doesNotContain("XYZ");
+    }
+}
