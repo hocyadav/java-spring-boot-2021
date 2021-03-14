dafbb3efe9b05a0c16d3b1f10810fd3c7c33c012
Hariom Yadav <hoc.yadav@gmail.com>
2021-03-14 09:22:11 +0530

    dao added

---
+++ entity_relation_mm/src/main/java/io/hari/demo/dao/ShowDao.java
@@ -0,0 +1,8 @@
+package io.hari.demo.dao;
+
+import io.hari.demo.entity.onetomanybi.Show;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShowDao extends BaseDao<Show> {
+}
