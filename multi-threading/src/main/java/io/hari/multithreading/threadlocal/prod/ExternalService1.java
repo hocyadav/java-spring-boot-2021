50412c2a6bc1587b163d3f241e78de6b4c60d657
Hariom Yadav <hoc.yadav@gmail.com>
2021-03-19 11:23:27 +0530

    ThreadLocal

---
+++ multi-threading/src/main/java/io/hari/multithreading/threadlocal/prod/ExternalService1.java
@@ -0,0 +1,12 @@
+package io.hari.multithreading.threadlocal.prod;
+
+/**
+ * @Author Hariom Yadav
+ * @create 19-03-2021
+ */
+public class ExternalService1 {
+
+    public String service1() {
+        return "service1";
+    }
+}
---
+++ multi-threading/src/main/java/io/hari/multithreading/threadlocal/prod/ExternalService2.java
@@ -0,0 +1,12 @@
+package io.hari.multithreading.threadlocal.prod;
+
+/**
+ * @Author Hariom Yadav
+ * @create 19-03-2021
+ */
+public class ExternalService2 {
+
+    public String service2() {
+        return "service2";
+    }
+}
