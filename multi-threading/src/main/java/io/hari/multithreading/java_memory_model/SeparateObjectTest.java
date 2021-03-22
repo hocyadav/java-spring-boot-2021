aab3608cd42d7b8db79aa18fab88e6ec0af10a74
Hariom Yadav <hoc.yadav@gmail.com>
2021-03-22 11:52:18 +0530

    java memory model - shared object - put inside synchronized block

--- multi-threading/src/main/java/io/hari/multithreading/java_memory_model/SeparateObjectTest.java
+++ multi-threading/src/main/java/io/hari/multithreading/java_memory_model/SeparateObjectTest.java
@@ -6,11 +6,10 @@ package io.hari.multithreading.java_memory_model;
  */
 public class SeparateObjectTest {
     public static void main(String[] args) {
-        Runnable runnable1 = new MyRunnable();
-        Runnable runnable2 = new MyRunnable();
-
-        final Thread thread1 = new Thread(runnable1, "Thread 1");
-        final Thread thread2 = new Thread(runnable2, "Thread 2");
+        Runnable runnable1      = new MyRunnable();
+        Runnable runnable2      = new MyRunnable();
+        final Thread thread1    = new Thread(runnable1, "THREAD_1");
+        final Thread thread2    = new Thread(runnable2, "THREAD_2");
         thread1.start();
         thread2.start();
     }
