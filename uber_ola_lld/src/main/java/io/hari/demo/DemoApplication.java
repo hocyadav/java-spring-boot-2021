fb83ea0ba6785cd6db5e68b0b69ab8ab32dc0116
Hariom Yadav <hoc.yadav@gmail.com>
2021-04-02 10:17:45 +0530

    lld for uber ola

---
+++ uber_ola_lld/src/main/java/io/hari/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package io.hari.demo;
+
+import io.hari.demo.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.List;
+import java.util.Map;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Autowired
+	AppConfig config;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("config = " + config);
+	}
+}
