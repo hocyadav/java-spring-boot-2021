d07d3dafc7077c03d403d119a73b3817a8fdc71c
Hariom Yadav <hoc.yadav@gmail.com>
2021-04-02 12:11:22 +0530

    dao

---
+++ uber_ola_lld/src/main/java/io/hari/demo/convertor/TripStatus.java
@@ -0,0 +1,12 @@
+package io.hari.demo.convertor;
+
+/**
+ * @Author Hariom Yadav
+ * @create 02-04-2021
+ */
+public enum TripStatus {
+    STARTED_FROM_SOURCE,
+    COMPLETED,
+    CANCEL_BY_USER,
+    CANCEL_BY_DRIVER,
+}
