494f318e904bbb0226639b83b7bb2c13305e31fc
Hariom Yadav <hoc.yadav@gmail.com>
2021-04-05 08:10:44 +0530

    scala sample maven based project

---
+++ scala-maven/src/main/scala/MyFirstObject.scala
@@ -0,0 +1,28 @@
+/**
+ * @Author Hariom Yadav
+ * @create 05-04-2021
+ * https://www.youtube.com/watch?v=UShLRRyZ0nk&list=PL3PnJuCKxQABKDHQAm9DpEN-NFBmvCfcM&index=2
+ */
+object MyFirstObject extends App {
+  var a = 10;
+  println(s"a = ${a}");
+  println(a);
+  println("hello world");
+
+  val x = 1 + 2;
+  println(x + "  - - ")
+
+  val c: Int = 10;
+  println(c)
+
+  val str: String = "hariom yadav";
+  println(str);
+
+  val ch: Char = 'h';
+  println(ch);
+
+  val t: Boolean = true;
+  val t2: Boolean = false;
+  println(t + " " + t2);
+
+}
