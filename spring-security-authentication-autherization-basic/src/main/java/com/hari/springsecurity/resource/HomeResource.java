f6dfc172dc24d19ecde134cc5004adf74eca4f32
Hariom Yadav <hoc.yadav@gmail.com>
2021-04-07 12:23:43 +0530

    added auth details

--- spring-security-authentication-autherization-basic/src/main/java/com/hari/springsecurity/resource/HomeResource.java
+++ spring-security-authentication-autherization-basic/src/main/java/com/hari/springsecurity/resource/HomeResource.java
@@ -1,5 +1,7 @@
 package com.hari.springsecurity.resource;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,18 +11,30 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HomeResource {
-    @GetMapping ("/")
+    @GetMapping("/")
     public String home() {
+        extracted();
         return "<h1> welcome </h1>";
     }
 
-    @GetMapping ("/user")
+    @GetMapping("/user")
     public String user() {
+        extracted();
         return "<h1> welcome user</h1>";
     }
 
-    @GetMapping ("/admin")
+    @GetMapping("/admin")
     public String admin() {
+        extracted();
         return "<h1> welcome admin</h1>";
+    }
+
+    private void extracted() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("auth.getPrincipal() = " + auth.getPrincipal());
+        System.out.println("auth.getName() = " + auth.getName());
+        System.out.println("auth.getDetails() = " + auth.getDetails());
+        System.out.println("auth.getAuthorities() = " + auth.getAuthorities());
+        System.out.println("auth.getCredentials() = " + auth.getCredentials());
     }
 }
