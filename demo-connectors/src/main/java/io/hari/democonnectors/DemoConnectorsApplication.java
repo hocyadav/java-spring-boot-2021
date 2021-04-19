29288167cacf5fa03ba8e977be7aae552ea9b35b
Hariom Yadav <hoc.yadav@gmail.com>
2021-04-19 13:13:58 +0530

    update

---
+++ demo-connectors/src/main/java/io/hari/democonnectors/DemoConnectorsApplication.java
@@ -0,0 +1,13 @@
+package io.hari.democonnectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConnectorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConnectorsApplication.class, args);
+	}
+
+}
---
+++ demo-connectors/src/test/java/io/hari/democonnectors/DemoConnectorsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hari.democonnectors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConnectorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
