61e107602599bb2a7aea2f9fa9ea07ae47d905b7
Hariom Yadav <hoc.yadav@gmail.com>
2021-04-19 22:33:02 +0530

    dao

---
+++ leetcode_lld/src/main/java/io/hari/demo/dao/NotificationDao.java
@@ -0,0 +1,13 @@
+package io.hari.demo.dao;
+
+import io.hari.demo.entity.Notification;
+import io.hari.demo.entity.Question;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author Hariom Yadav
+ * @create 19-04-2021
+ */
+@Repository
+public interface NotificationDao extends BaseDao<Notification>{
+}
