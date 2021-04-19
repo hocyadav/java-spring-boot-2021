22798889f459e5c71af18905795268ea093b2122
Hariom Yadav <hoc.yadav@gmail.com>
2021-04-19 22:32:58 +0530

    dao

---
+++ leetcode_lld/src/main/java/io/hari/demo/dao/ContestDao.java
@@ -0,0 +1,13 @@
+package io.hari.demo.dao;
+
+import io.hari.demo.entity.Contest;
+import io.hari.demo.entity.User;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author Hariom Yadav
+ * @create 19-04-2021
+ */
+@Repository
+public interface ContestDao extends BaseDao<Contest>{
+}
