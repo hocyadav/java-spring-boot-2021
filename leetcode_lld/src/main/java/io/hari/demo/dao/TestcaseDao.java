5772de7c23c365f835cd4b6c72e45d2782308e00
Hariom Yadav <hoc.yadav@gmail.com>
2021-04-19 22:33:13 +0530

    dao

---
+++ leetcode_lld/src/main/java/io/hari/demo/dao/TestcaseDao.java
@@ -0,0 +1,13 @@
+package io.hari.demo.dao;
+
+import io.hari.demo.entity.Question;
+import io.hari.demo.entity.TestCase;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author Hariom Yadav
+ * @create 19-04-2021
+ */
+@Repository
+public interface TestcaseDao extends BaseDao<TestCase>{
+}
