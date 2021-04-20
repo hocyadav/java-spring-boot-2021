d571f2841da2aa072ce6ebe078b2a4329111435c
Hariom Yadav <hoc.yadav@gmail.com>
2021-04-20 09:11:45 +0530

    dao

---
+++ leetcode_lld/src/main/java/io/hari/demo/dao/ReplyDao.java
@@ -0,0 +1,13 @@
+package io.hari.demo.dao;
+
+import io.hari.demo.entity.Discussion;
+import io.hari.demo.entity.Reply;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author Hariom Yadav
+ * @create 19-04-2021
+ */
+@Repository
+public interface ReplyDao extends BaseDao<Reply>{
+}
