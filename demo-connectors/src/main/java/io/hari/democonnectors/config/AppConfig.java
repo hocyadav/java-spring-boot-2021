3db4241f7a2e19cce5b97e88e28230fda24aa526
Hariom Yadav <hoc.yadav@gmail.com>
2021-04-22 08:41:25 +0530

    updated config

---
+++ demo-connectors/src/main/java/io/hari/democonnectors/config/AppConfig.java
@@ -0,0 +1,45 @@
+package io.hari.democonnectors.config;
+
+import lombok.*;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @Author hayadav
+ * @create 4/20/2021
+ */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@ConfigurationProperties(prefix = "app-config")
+public class AppConfig {
+    String simpleKey;
+    Database source;
+    Database target;
+
+    @Getter
+    @Setter
+    @ToString
+    public static class Database {
+        String datasource;
+        String url;
+        String username;
+        String password;
+        Schema schema;
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class Schema {
+        String schemaName;
+        String tableName;
+        List<String> tableColumns;
+    }
+}
+
