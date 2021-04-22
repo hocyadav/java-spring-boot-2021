7065fe586fbf10bef2189b9f0a5042fb75f6c8f4
Hariom Yadav <hoc.yadav@gmail.com>
2021-04-22 08:41:36 +0530

    updated

--- demo-connectors/src/main/java/io/hari/democonnectors/DemoConnectorsApplication.java
+++ demo-connectors/src/main/java/io/hari/democonnectors/DemoConnectorsApplication.java
@@ -1,13 +1,30 @@
 package io.hari.democonnectors;
 
+import io.hari.democonnectors.config.AppConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class DemoConnectorsApplication {
+@RequiredArgsConstructor
+public class DemoConnectorsApplication implements CommandLineRunner {
+	final AppConfig appConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoConnectorsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("appConfig = " + appConfig);
+		final AppConfig.Database source = appConfig.getSource();
+		System.out.println("source = " + source);
+		final AppConfig.Schema schema = source.getSchema();
+		System.out.println("schema = " + schema);
+		final List<String> columns = schema.getTableColumns();
+		System.out.println("columns = " + columns);
+	}
 }
