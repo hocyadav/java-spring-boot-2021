fc8e18deb54cef0b897be3382207fd422dc849a6
Hariom Yadav <hoc.yadav@gmail.com>
2021-04-30 17:35:44 +0530

    cloud db test config

--- demo-connectors/src/main/java/io/hari/democonnectors/config/AppConfig.java
+++ demo-connectors/src/main/java/io/hari/democonnectors/config/AppConfig.java
@@ -21,6 +21,7 @@ public class AppConfig {
     String simpleKey;
     Database source;
     Database target;
+    RemoteMySQL remotemySQL;
 
     @Getter
     @Setter
@@ -40,6 +41,17 @@ public class AppConfig {
         String schemaName;
         String tableName;
         List<String> tableColumns;
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class RemoteMySQL {
+        String username;
+        String password;
+        String server;
+        Integer port;
+        String databaseSchemaName;
     }
 }
 
