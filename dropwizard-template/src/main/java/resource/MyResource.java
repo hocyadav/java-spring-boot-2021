21fa10bad6111cb3442934863730e34b8989fc8c
Hariom Yadav <hoc.yadav@gmail.com>
2021-05-22 12:52:49 +0530

    dropwizard-template - @Timed metrics added http://localhost:8081/metrics

--- dropwizard-template/src/main/java/resource/MyResource.java
+++ dropwizard-template/src/main/java/resource/MyResource.java
@@ -1,8 +1,10 @@
 package resource;
 
+import com.codahale.metrics.annotation.Timed;
 import config.AppConfig;
 import entity.PersonEntity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -18,11 +20,14 @@ import java.util.Optional;
 @Path("/test-api")
 @Produces(MediaType.APPLICATION_JSON)
 @RequiredArgsConstructor
+@Slf4j
 public class MyResource {
     private final AppConfig config;
 
+    @Timed //http://localhost:8081/metrics open and we can see how many times this end point is called and other imp info
     @GET
     public PersonEntity getResource(@QueryParam("name")Optional<String> name) {
+        log.info("name = " + name);
         final String fetchName = name.orElseGet(() -> "hello stranger");
         return PersonEntity.builder().name(fetchName).address(config.getLocation()).build();
     }
