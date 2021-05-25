a216bb756e4b6998b3d80c03fdd3184753118ae9
Hariom Yadav <hoc.yadav@gmail.com>
2021-05-25 07:37:15 +0530

    logback - sl4j log implementation

--- dropwizard-template/src/main/java/resource/MyResource.java
+++ dropwizard-template/src/main/java/resource/MyResource.java
@@ -5,6 +5,8 @@ import config.AppConfig;
 import entity.PersonEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -20,14 +22,16 @@ import java.util.Optional;
 @Path("/test-api")
 @Produces(MediaType.APPLICATION_JSON)
 @RequiredArgsConstructor
-@Slf4j
+//@Slf4j
 public class MyResource {
+    final Logger log = LoggerFactory.getLogger(MyResource.class);//dropwizard logback sl4j implementation : http://www.slf4j.org/manual.html
+
     private final AppConfig config;
 
     @Timed //http://localhost:8081/metrics open and we can see how many times this end point is called and other imp info
     @GET
     public PersonEntity getResource(@QueryParam("name")Optional<String> name) {
-        log.info("name = " + name);
+        log.trace("name = " + name);
         final String fetchName = name.orElseGet(() -> "hello stranger");
         return PersonEntity.builder().name(fetchName).address(config.getLocation()).build();
     }
