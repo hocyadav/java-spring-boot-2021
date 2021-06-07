997a7e4af24aed2824fde7b06fe90aaa456429c3
Hariom Yadav <yadav.hariom@BLRETV-C02F52AP.local>
2021-06-07 19:10:07 +0530

    update

--- google-guice/src/main/java/io/hari/googleguice/simple2/MyName.java
+++ google-guice/src/main/java/io/hari/googleguice/simple2/MyName.java
@@ -9,5 +9,5 @@ import java.lang.annotation.Target;
 
 @BindingAnnotation
 @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD}) @Retention(RetentionPolicy.RUNTIME)
-public @interface MyName {
+public @interface MyName { //https://www.youtube.com/watch?v=xfeEjAj2Rgs&list=PLp0ed20U4R4jknb4xYdhx3yJn5RhWECxn&index=9
 }
