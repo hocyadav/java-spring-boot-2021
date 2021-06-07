22cba35f4e9f89ff98fab8392e3af09b45b39576
Hariom Yadav <yadav.hariom@BLRETV-C02F52AP.local>
2021-06-07 18:40:10 +0530

    refactored

--- google-guice/src/main/java/io/hari/googleguice/simple/MyAbstractModule.java
+++ google-guice/src/main/java/io/hari/googleguice/simple/MyAbstractModule.java
@@ -8,8 +8,8 @@ import io.hari.googleguice.simple2.impl.MyShapeImpl2;
 public class MyAbstractModule extends AbstractModule {
     @Override
     protected void configure() {
-//        super.configure();//working
+//        super.configure();//working : this is for simple class that is concrete
 //        bind(IShape.class).to(MyShapeImpl1.class);//working
-        bind(IShape.class).to(MyShapeImpl2.class);//working
+        bind(IShape.class).to(MyShapeImpl2.class);//working : this is use for interface -> to concrete impl class, or concrete parent -> concrete child class
     }
 }
--- google-guice/src/main/java/io/hari/googleguice/simple2/MyShapeRequest.java
+++ google-guice/src/main/java/io/hari/googleguice/simple2/MyShapeRequest.java
@@ -6,7 +6,7 @@ import io.hari.googleguice.simple2.impl.IShape;
 public class MyShapeRequest {
     IShape shape;
 
-    @Inject
+    @Inject//this will go to AbstractModule class to see if binding is present or not for this interface : https://www.youtube.com/watch?v=tjHjHWNANGo&list=PLp0ed20U4R4jknb4xYdhx3yJn5RhWECxn&index=8
     public MyShapeRequest(IShape shape) {
         this.shape = shape;
     }
