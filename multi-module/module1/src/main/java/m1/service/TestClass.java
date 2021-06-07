8c31d26031db7ef658dd45175976e4124f283807
Hariom Yadav <yadav.hariom@BLRETV-C02F52AP.local>
2021-06-07 15:36:55 +0530

    multi-module maven project

---
+++ multi-module/module1/src/main/java/m1/service/TestClass.java
@@ -0,0 +1,7 @@
+package m1.service;
+
+public class TestClass {
+    public static String reverse() {
+        return "hariom";
+    }
+}
---
+++ multi-module/module2/src/main/java/m2/service/Test2Class.java
@@ -0,0 +1,10 @@
+package m2.service;
+
+import m1.service.TestClass;
+
+public class Test2Class {
+    public static void main(String[] args) {
+        String reverse = TestClass.reverse();
+        System.out.println("reverse = " + reverse);
+    }
+}
