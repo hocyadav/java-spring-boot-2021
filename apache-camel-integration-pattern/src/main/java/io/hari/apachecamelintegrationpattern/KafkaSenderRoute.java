d4b1e841a5161c865dc0d952157833b15d46bacb
Hariom Yadav <hoc.yadav@gmail.com>
2021-06-18 10:59:47 +0530

    docker and kafka setup steps

--- apache-camel-integration-pattern/src/main/java/io/hari/apachecamelintegrationpattern/KafkaSenderRoute.java
+++ apache-camel-integration-pattern/src/main/java/io/hari/apachecamelintegrationpattern/KafkaSenderRoute.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Component;
 /**
  * @author Hariom Yadav
  * @since 18/06/21
+ * 1 docker install steps : 3min -> https://youtu.be/LtooWDUL1Js
+ * 2 kafka install 2min using docker compose -> see docker-compose yml file for zookeeper and kafka ->  $docker-compose up
+ * 3 : add dependency for kafka connector
+ * 3.a add camel-kafka application properties for apache camel (where kafka is runing url+port): camel.component.kafka.brokers=localhost:9092
  */
 @Component
 public class KafkaSenderRoute extends RouteBuilder {
