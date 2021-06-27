b0823c5924ba59b64b5ba2c77c9d791baee1a1e9
Hariom Yadav <hoc.yadav@gmail.com>
2021-06-27 09:50:15 +0530

    run in separate thread - subscribe on, publish on both will take Scheduler instance

---
+++ reactive-framework/src/main/java/io/hari/reactiveframework/JavaReactiveFrameworkApplication.java
@@ -0,0 +1,13 @@
+package io.hari.reactiveframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReactiveFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaReactiveFrameworkApplication.class, args);
+	}
+
+}
---
+++ reactive-framework/src/test/java/io/hari/reactiveframework/ReactiveFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hari.reactiveframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
