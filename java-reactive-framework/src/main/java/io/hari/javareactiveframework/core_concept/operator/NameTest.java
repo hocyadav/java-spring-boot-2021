b03627a4297a714c4a8f18f9d59787dc433ad055
Hariom Yadav <hoc.yadav@gmail.com>
2021-07-22 13:39:50 +0530

    Name and Metrics method

--- java-reactive-framework/src/main/java/io/hari/javareactiveframework/core_concept/operator/NameTest.java
+++ java-reactive-framework/src/main/java/io/hari/javareactiveframework/core_concept/operator/NameTest.java
@@ -6,9 +6,11 @@ import reactor.core.publisher.Flux;
 public class NameTest {
     @Test
     public void test() {
-        Flux.range(1, 100)
-                .name("flux-stream")//??
+        Flux.range(1, 3)
+                .metrics() //??
                 .log()
+                .name("flux-stream")//??
+//                .log()
                 .subscribe(
                         data -> System.out.println("data = " + data),
                         err -> System.out.println("err.getMessage() = " + err.getMessage()),
