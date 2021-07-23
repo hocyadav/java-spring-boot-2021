63da4009a5447b351c7f112d9a3bd711beaab8d7
Hariom Yadav <hoc.yadav@gmail.com>
2021-07-23 10:12:39 +0530

    Name

--- java-reactive-framework/src/main/java/io/hari/javareactiveframework/core_concept/operator/NameTest.java
+++ java-reactive-framework/src/main/java/io/hari/javareactiveframework/core_concept/operator/NameTest.java
@@ -1,17 +1,30 @@
 package io.hari.javareactiveframework.core_concept.operator;
 
 import org.junit.Test;
+import reactor.core.Scannable;
 import reactor.core.publisher.Flux;
 
 public class NameTest {
     @Test
     public void test() {
-        Flux.range(1, 3)
-                .metrics() //??
-                .log()
-                .name("flux-stream")//??
+        Flux<Integer> upstreamFlux = Flux.range(1, 3)
+//                .metrics() //??
 //                .log()
-                .subscribe(
+                .name("flux-stream");//??
+//                .log();
+
+
+        Scannable scannable = Scannable.from(upstreamFlux);
+        System.out.println("scannable = " + scannable);
+        String name = scannable.name();
+        System.out.println("name = " + name);//flux-stream
+
+        Flux<Integer> integerFlux = upstreamFlux.name("new-name");
+        Scannable scannable1 = Scannable.from(integerFlux);
+        System.out.println("scannable1.name() = " + scannable1.name());// new-name
+
+
+        upstreamFlux.subscribe(
                         data -> System.out.println("data = " + data),
                         err -> System.out.println("err.getMessage() = " + err.getMessage()),
                         () -> System.out.println("DONE signal")
