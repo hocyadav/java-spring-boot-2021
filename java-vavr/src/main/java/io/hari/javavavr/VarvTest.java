086fb722e33c3ab26dd4edb9dd78dc23d71a5bd6
Hariom Yadav <hoc.yadav@gmail.com>
2021-11-03 19:15:37 +0530

    varv test

--- java-vavr/src/main/java/io/hari/javavavr/VarvTest.java
+++ java-vavr/src/main/java/io/hari/javavavr/VarvTest.java
@@ -8,9 +8,11 @@ import io.vavr.control.Try;
 import org.junit.Test;
 
 import java.net.URI;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class VarvTest {
 
@@ -264,6 +266,11 @@ public class VarvTest {
         //java mutable to java immutable
         java.util.List<Integer> list = List.ofAll(javaMutable)//java varv immutable
                 .asJava();//java 8 immutable
+
+//        Map<Integer, java.util.List<Integer>> listMap =
+//                originalImmutableList.collect(Collectors.groupingBy(i -> i.intValue()));
+//        System.out.println("listMap = " + listMap);
+
 
     }
 
