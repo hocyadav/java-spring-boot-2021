2e77d2d258589c8b3e3182e1c44c287e7d6957cd
Hariom Yadav <hoc.yadav@gmail.com>
2021-11-04 09:23:11 +0530

    updated

--- java-reactive-programming-course-master/src/main/java/com/rp/sec09/Lec02OverlapAndDrop.java
+++ java-reactive-programming-course-master/src/main/java/com/rp/sec09/Lec02OverlapAndDrop.java
@@ -8,8 +8,8 @@ import java.time.Duration;
 public class Lec02OverlapAndDrop {
 
     public static void main(String[] args) {
-        //use case 1: check for some patterns in events (required windows size data, so subsequence group need to know info about each other)
-        //use case 2: sampling of events (only required few events/data)
+        //use case 1 (overlap): check for some patterns in events (required windows size data, so subsequence group need to know info about each other)
+        //use case 2 (drop): sampling of events (only required few events/data)
 
         //use case 1: small skip value : size > skip
         //use case 2: large skip value : size < skip
